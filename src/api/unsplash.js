5ef4c65821c2956f705b677bc3c10b0a6c7a1999
Daniel Chamorro <dan@dcitllc.com>
2018-12-01 22:48:47 -0500

    Added env variable for unslash

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization:
-      "Client-ID 9132acf7c6b1e121f2ea59aeb67ccee944fc7e281d855ccf95c2ed2012dd75c4"
+    Authorization: `Client-ID ${process.env.REACT_APP_UNSLASH_KEY}`
   }
 });
